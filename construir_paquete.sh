ff537e0a1ac559e567e9939b99cbfe1c59e37904
Alberto Fernandez <infjaf@gmail.com>
2016-12-19 23:23:54 +0100

    Corrige nombre de directorio

--- construir_paquete.sh
+++ construir_paquete.sh
@@ -12,7 +12,7 @@ echo $VERSION_AUTOFIRMA
 svn export https://github.com/albfernandez/clienteafirma/tags/v$VERSION_AUTOFIRMA autofirma-$VERSION_AUTOFIRMA
 tar zcvf autofirma_$VERSION_AUTOFIRMA.orig.tar.gz autofirma-$VERSION_AUTOFIRMA/
 #svn export https://github.com/albfernandez/clienteafirma-deb-package/trunk/ clientefirma-deb-package
-svn export https://github.com/albfernandez/clienteafirma-deb-package/tags/v$VERSION_DEBIAN clientefirma-deb-package
-mv clientefirma-deb-package/debian/ autofirma-$VERSION_AUTOFIRMA/
+svn export https://github.com/albfernandez/clienteafirma-deb-package/tags/v$VERSION_DEBIAN clienteafirma-deb-package
+mv clienteafirma-deb-package/debian/ autofirma-$VERSION_AUTOFIRMA/
 cd autofirma-$VERSION_AUTOFIRMA
 dpkg-buildpackage
