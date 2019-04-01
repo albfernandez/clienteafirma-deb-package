4e8009165e260b4c28e6d762d81ae9f1b491be0f
Alberto Fernandez <infjaf@gmail.com>
2019-04-01 21:20:45 +0200

    Release 1.6.3
    Fixes #6

--- construir_paquete.sh
+++ construir_paquete.sh
@@ -15,4 +15,5 @@ tar zcvf autofirma_$VERSION_AUTOFIRMA.orig.tar.gz autofirma-$VERSION_AUTOFIRMA/
 svn export https://github.com/albfernandez/clienteafirma-deb-package/tags/v$VERSION_DEBIAN clienteafirma-deb-package
 mv clienteafirma-deb-package/debian/ autofirma-$VERSION_AUTOFIRMA/
 cd autofirma-$VERSION_AUTOFIRMA
+mvn clean package -Dmaven.test.skip=true install
 dpkg-buildpackage
