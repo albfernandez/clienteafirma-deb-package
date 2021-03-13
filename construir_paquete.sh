34fd63e85a489a8d168a568daf489e286ff86e9d
Alberto Fernandez <infjaf@gmail.com>
2021-03-13 13:21:41 +0100

    release 1.7
    Fixes #8
    Fixes #11

--- construir_paquete.sh
+++ construir_paquete.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 VERSION" >&2
-  exit 1
-fi
-
-VERSION_DEBIAN=$1
-VERSION_AUTOFIRMA=`echo $VERSION_DEBIAN | sed -e s/-.*//g`
+VERSION_JMULTICARD=1.6.202103.1
+VERSION_AUTOFIRMA=1.7.202103.1
+VERSION_DEBIAN=1.7.202103.1-1
 echo $VERSION_AUTOFIRMA
+
+
+# Para incluir los ultimos cambios del DNI 3 unamos una versión aún no liberada de JMULTICARD 
+svn export https://github.com/albfernandez/jmulticard/tags/v.$VERSION_JMULTICARD jmulticard-$VERSION_JMULTICARD
+cd jmulticard-$VERSION_JMULTICARD
+mvn clean package -Dmaven.test.skip=true install
+cd ..
+
 
 svn export https://github.com/albfernandez/clienteafirma/tags/v$VERSION_AUTOFIRMA autofirma-$VERSION_AUTOFIRMA
 tar zcvf autofirma_$VERSION_AUTOFIRMA.orig.tar.gz autofirma-$VERSION_AUTOFIRMA/
-#svn export https://github.com/albfernandez/clienteafirma-deb-package/trunk/ clientefirma-deb-package
-svn export https://github.com/albfernandez/clienteafirma-deb-package/tags/v$VERSION_DEBIAN clienteafirma-deb-package
+svn export https://github.com/albfernandez/clienteafirma-deb-package/tags/v.$VERSION_DEBIAN clienteafirma-deb-package
 mv clienteafirma-deb-package/debian/ autofirma-$VERSION_AUTOFIRMA/
 cd autofirma-$VERSION_AUTOFIRMA
 mvn clean package -Dmaven.test.skip=true install
