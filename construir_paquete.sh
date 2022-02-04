090ec751482d6f21e598ef623771d9a583655a8b
Alberto Fernandez <infjaf@gmail.com>
2022-02-04 21:53:07 +0100

    Nueva release 2022-02-04
    Signed-off-by: Alberto Fernandez <infjaf@gmail.com>

--- construir_paquete.sh
+++ construir_paquete.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-VERSION_JMULTICARD=1.6.202103.1
-VERSION_AUTOFIRMA=1.7.202103.2
-VERSION_DEBIAN=1.7.202103.2-2
+VERSION_JMULTICARD=1.7.20220204.1
+VERSION_AUTOFIRMA=1.7.202202.2
+VERSION_DEBIAN=1.7.202202.2-1
+VERSION_EXTERNAL=20220204.1
 echo $VERSION_AUTOFIRMA
 
 
@@ -12,11 +13,18 @@ cd jmulticard-$VERSION_JMULTICARD
 mvn clean package -Dmaven.test.skip=true install
 cd ..
 
+svn export https://github.com/albfernandez/clienteafirma-external/tags/v.$VERSION_EXTERNAL clienteafirma-external-$VERSION_EXTERNAL
+cd clienteafirma-external-$VERSION_EXTERNAL
+mvn clean package -Dmaven.test.skip=true install
+cd ..
 
 svn export https://github.com/albfernandez/clienteafirma/tags/v.$VERSION_AUTOFIRMA autofirma-$VERSION_AUTOFIRMA
 tar zcvf autofirma_$VERSION_AUTOFIRMA.orig.tar.gz autofirma-$VERSION_AUTOFIRMA/
+
 svn export https://github.com/albfernandez/clienteafirma-deb-package/tags/v.$VERSION_DEBIAN clienteafirma-deb-package
-mv clienteafirma-deb-package/debian/ autofirma-$VERSION_AUTOFIRMA/
+cp -a clienteafirma-deb-package/debian/ autofirma-$VERSION_AUTOFIRMA/
+
+
 cd autofirma-$VERSION_AUTOFIRMA
-mvn clean package -Dmaven.test.skip=true install
+
 dpkg-buildpackage
