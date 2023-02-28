8ee5bd9d0dd892b7fe22cc6fed5eb9c64fe04e99
Alberto Fernandez <infjaf@gmail.com>
2023-02-28 19:44:03 +0100

    Actualizado script de construccion

--- construir_paquete.sh
+++ construir_paquete.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-VERSION_JMULTICARD=1.7.20220204.1
-VERSION_AUTOFIRMA=1.7.202202.2
-VERSION_DEBIAN=1.7.202202.2-1
-VERSION_EXTERNAL=20220204.1
+VERSION_JMULTICARD=1.8.202301.1
+VERSION_AUTOFIRMA=1.8.202301.1
+VERSION_EXTERNAL=202301.1
 echo $VERSION_AUTOFIRMA
 
+# Eliminamos los archivos generados previamente
+rm -f autofirma-1.8* autofirma_1.8*
 
 # Para incluir los ultimos cambios del DNI 3 unamos una versión aún no liberada de JMULTICARD 
 svn export https://github.com/albfernandez/jmulticard/tags/v.$VERSION_JMULTICARD jmulticard-$VERSION_JMULTICARD
@@ -21,9 +22,7 @@ cd ..
 svn export https://github.com/albfernandez/clienteafirma/tags/v.$VERSION_AUTOFIRMA autofirma-$VERSION_AUTOFIRMA
 tar zcvf autofirma_$VERSION_AUTOFIRMA.orig.tar.gz autofirma-$VERSION_AUTOFIRMA/
 
-svn export https://github.com/albfernandez/clienteafirma-deb-package/tags/v.$VERSION_DEBIAN clienteafirma-deb-package
-cp -a clienteafirma-deb-package/debian/ autofirma-$VERSION_AUTOFIRMA/
-
+cp -a debian/ autofirma-$VERSION_AUTOFIRMA/
 
 cd autofirma-$VERSION_AUTOFIRMA
 
